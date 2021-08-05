94e90bfe6f9b4267f9b9b13c2f577bfa3b813ba0
fgeorgescu <franco.georgescu@mercadolibre.com>
2021-08-05 16:00:50 -0300

    test

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const coffeeHandler = require("./handlers/coffee")
 const nahueHandler = require("./handlers/nahue")
 
+const express = require('express')
+const app = express()
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
